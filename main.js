904094aad52ec7c6f8e3993ef4f406f3d642d88f
carolinerobbins <caroline.tawney@gmail.com>
2023-03-11 12:46:26 -0800

    created phrases and new function

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("Survivor Quote Generator");
+console.log("Trip Generator");
+let destination = ['Hawaii', 'Lake Tahoe', 'Switzerland', 'Fiji', 'New York City', 'Sydney']
+let transportation = ['walking','airplane','boat','car','train','biking']
+let activity = ['diving', 'surfing','skiing','hiking','seeing a show','eating at amazing food']
+function createSillyVacation(){
+    let randomDest = destination[Math.floor(Math.random()*6)];
+    let randomTrans = transportation[Math.floor(Math.random()*6)];
+    let randomAct = activity[Math.floor(Math.random()*6)];
+    console.log(`You are going on a trip to ${randomDest}. You will get there by ${randomTrans} and you will be ${randomAct}.`;
+}
+
+createSillyVacation();
